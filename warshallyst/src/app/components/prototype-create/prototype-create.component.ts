24b823758df33b0006e5b0fcb887176a3bccb009
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-04-26 21:44:07 +0100

    Create a placeholder graph

--- warshallyst/src/app/components/prototype-create/prototype-create.component.ts
+++ warshallyst/src/app/components/prototype-create/prototype-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Layout, Edge, Node, MiniMapPosition } from '@swimlane/ngx-graph';
 
 @Component({
   selector: 'app-prototype-create',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./prototype-create.component.scss']
 })
 export class PrototypeCreateComponent {
+
+  view: [number, number] = [1124, 600];
+  miniMapPosition:MiniMapPosition = MiniMapPosition.UpperLeft;
+
+  nodes: Node[] = [
+    {
+      id: 'node_1',
+      label: 'W1'
+    }, {
+      id: 'node_2',
+      label: 'C1'
+    }, {
+      id: 'node_3',
+      label: 'C2'
+    }, {
+      id: 'node_4',
+      label: 'W2'
+    }
+  ];
+
+
+  links: Edge[] = [
+    {
+      id: 'link_1',
+      source: 'node_1',
+    target: 'node_2',
+      label: '10'
+    }, {
+      id: 'link_2',
+      source: 'node_1',
+      target: 'node_3',
+      label: '5'
+    }, {
+      id: 'link_3',
+      source: 'node_2',
+      target: 'node_4',
+      label: '20'
+    }
+  ];
 
 }
