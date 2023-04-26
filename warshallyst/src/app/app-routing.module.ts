01d3352a8fdad7568a030d7db2bac717ebf97e39
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-04-26 21:40:36 +0100

    Add "demo-create" route

--- warshallyst/src/app/app-routing.module.ts
+++ warshallyst/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PrototypeCreateComponent } from './components/prototype-create/prototype-create.component';
+
+const routes: Routes = [
+  {path:'demo-create', title: 'Demo Create', component: PrototypeCreateComponent},
+  {path:'', redirectTo: 'demo-create', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
