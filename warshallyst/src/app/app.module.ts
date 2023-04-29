e24ed4917172d6ae2fe0cf798873dd4ca547c89b
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-04-29 19:55:45 +0100

    Import FormsModule

--- warshallyst/src/app/app.module.ts
+++ warshallyst/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxGraphModule } from '@swimlane/ngx-graph';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,9 +14,10 @@ import { PrototypeCreateComponent } from './components/prototype-create/prototyp
     PrototypeCreateComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    BrowserAnimationsModule,
     NgxGraphModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
